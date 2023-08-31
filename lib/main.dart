263d569260947f7b216cb7253a09f53ada1a7064
JoaoPauloUbaF <joaopaulouferreira@gmail.com>
2023-08-31 16:09:46 -0300

    alterações grid, separação de widgets

--- lib/main.dart
+++ lib/main.dart
@@ -27,6 +27,10 @@ class _GameOfLifeState extends State<GameOfLife> {
 
   Timer? timer;
 
+  Widget _someSpace = const SizedBox(
+    height: 20,
+  );
+
   @override
   void initState() {
     timer = Timer.periodic(const Duration(milliseconds: 1000), (timer) {
@@ -51,56 +55,74 @@ class _GameOfLifeState extends State<GameOfLife> {
       appBar: AppBar(
         title: Text('Generation: $_generation'),
       ),
-      body: ListView(
-        children: [
-          TextField(
-            decoration: const InputDecoration(
-              hintText: 'Enter number of rows',
+      body: SizedBox(
+        height: MediaQuery.of(context).size.height,
+        child: ListView(
+          children: [
+            Center(
+              child: Column(
+                children: [
+                  _someSpace,
+                  const Text('Enter the dimension'),
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: TextField(
+                      decoration: const InputDecoration(
+                        hintText: ' Example: 4 will create a 4x4 grid',
+                      ),
+                      keyboardType: TextInputType.number,
+                      onSubmitted: (value) {
+                        setState(() {
+                          _generation = 0;
+                          grid.rows = int.parse(value);
+                          grid.columns = int.parse(value);
+                          grid.initGrid();
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
             ),
-            keyboardType: TextInputType.number,
-            onSubmitted: (value) {
-              setState(() {
-                _generation = 0;
-                grid.rows = int.parse(value);
-                grid.columns = int.parse(value);
-                grid.initGrid();
-              });
-            },
-          ),
-          GridWidget(grid: grid),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                onPressed: () {
-                  if (timer!.isActive) {
-                    return;
-                  }
-                  timer = Timer.periodic(const Duration(seconds: 1), (timer) {
-                    _tick();
-                  });
-                },
-                icon: const Icon(Icons.play_arrow),
-                color: Colors.green,
-              ),
-              IconButton(
-                onPressed: () => {
-                  timer!.isActive ? timer!.cancel() : null,
-                  setState(() {
-                    _generation = 0;
-                    grid.initGrid();
-                  })
-                },
-                icon: const Icon(Icons.stop),
-              ),
-              IconButton(
-                onPressed: () => timer!.isActive ? timer!.cancel() : null,
-                icon: const Icon(Icons.pause),
-                color: Colors.orange,
-              ),
-            ],
-          )
-        ],
+            _someSpace,
+            Padding(
+              padding: const EdgeInsets.all(28.0),
+              child: GridWidget(grid: grid),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                  onPressed: () {
+                    if (timer!.isActive) {
+                      return;
+                    }
+                    timer = Timer.periodic(const Duration(seconds: 1), (timer) {
+                      _tick();
+                    });
+                  },
+                  icon: const Icon(Icons.play_arrow),
+                  color: Colors.green,
+                ),
+                IconButton(
+                  onPressed: () => {
+                    timer!.isActive ? timer!.cancel() : null,
+                    setState(() {
+                      _generation = 0;
+                      grid.initGrid();
+                    })
+                  },
+                  icon: const Icon(Icons.stop),
+                ),
+                IconButton(
+                  onPressed: () => timer!.isActive ? timer!.cancel() : null,
+                  icon: const Icon(Icons.pause),
+                  color: Colors.orange,
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
